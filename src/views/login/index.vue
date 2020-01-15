ce74599aa925a214b29ada8369058edf412a3cfa
lixiaolong123 <b2590841724@163.com>
2020-01-15 19:54:25 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    登录页
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
